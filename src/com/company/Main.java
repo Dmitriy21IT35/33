4608261baddab74c31ac1c46dd71b05fb6b28e78
Dmitriy21IT35 <97725909+Dmitriy21IT35@users.noreply.github.com>
2022-05-24 09:59:07 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,47 +4,58 @@ import java.util.Scanner;
 
 class Calculator {
     public static void main(String[] args) {
-        //ВВод польз в консоль
-        System.out.println ("ВВедите выражение");
-        Scanner sc = new Scanner (System.in);
-         //ПРИСВАИВАЕМ ПЕРЕМЕННЫЕ
-        int a;
-        int b;
-        String op;//для ввода значкоа операции  применили в строку
-        do {
-            String input = sc.next ();
-            String[] strings = input.split ("\\W");
-            String[] operator = input.split ("\\w");
-            try {
-                a = Integer.parseInt (strings[0]);
-                b = Integer.parseInt (strings[1]);
-                op = operator[operator.length - 1];
-                 PrintWriter operation = new PrintWriter ("C:\\rico\\ru.txt");//запись в блокнот файла полученный результат
-                operation.println (operation (a, b, op));
-                System.out.println ("Результат = " + operation (a, b, op));
-                operation.close ();
-            } catch (Exception e) {
-                System.out.println ("Ошибка ввода данных");
+      File doc = new File("C:\\rico\\rico.txt");
+            BufferedReader obj = new BufferedReader(new FileReader (doc));
+            String strng;
+            strng = obj.readLine();
+            System.out.println(strng);
+
+
+            //ПРИСВАИВАЕМ ПЕРЕМЕННЫЕ
+            int a;
+            int b;
+
+            String op;//для ввода значкоа операции  применили в строку
+            do {
+
+                String[] strings = strng.split ("\\W");
+                String[] operator = strng.split ("\\w");
+
+                try {
+                    a = Integer.parseInt (strings[0]);
+                    b = Integer.parseInt (strings[1]);
+                    op = operator[operator.length - 1];
+
+
+
+
+                    PrintWriter operation = new PrintWriter ("C:\\rico\\ru.txt");//запись в блокнот(файл)
+                    operation.println (operation (a, b, op));
+                    System.out.println (operation (a, b, op));
+                    operation.close ();
+                    break;
+                } catch (Exception e) {
+                    System.out.println ("Ошибка ввода данных");
+                }
+            } while (true);
+
+        }
+        //КЛАСС С ОПЕРАЦИЯМИ
+        private static double operation(double a, double b, String op) {
+
+            if ("*".equals (op)) {
+                return a * b;
+            } else if ("+".equals (op)) {
+                return a + b;
+            } else if ("-".equals (op)) {
+                return a - b;
+            } else if ("/".equals (op)) {
+                return a / b;
+            } else if ("^".equals (op)) {
+                return (int) Math.pow (a, b);
+            } else if ("%".equals (op)) {
+                return a % b;
             }
-        } while (true);
-    }
-    //КЛАСС С ОПЕРАЦИЯМИ
-    private static double operation(double a, double b, String op) {
-        new Scanner (System.in);
-        if ("*".equals (op)) {
-            return a * b;
-        } else if ("+".equals (op)) {
-            return a + b;
-        } else if ("-".equals (op)) {
-            return a - b;
-        } else if ("/".equals (op)) {
-            return a / b;
-        } else if ("^".equals (op)) {
-            return (int) Math.pow (a,b);
-        }
-        else if ("%".equals (op)) {
-            return a % b;
-        }
-        return 0;
+            return 0;
     }
 }
